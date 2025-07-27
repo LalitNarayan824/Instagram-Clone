acc0e738023c61eaaa5c610bd070416982363dc4
Lalit Narayan <mjolnirxlalit@gmail.com>
2025-07-27 19:55:43 +0530

    Update index.js

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"))
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://instagram-ogh1.onrender.com",
   credentials:true,
 }))
 
